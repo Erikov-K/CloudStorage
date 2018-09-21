ff3d738191bf98400ae219cbe4b8e0c057021c51
Ериков Константин Александрович <k.erikov@2gis.ru>
2018-09-21 13:04:19 +0700

    "Added Maven with three modules"

---
+++ client/src/main/java/com/geekbrains/cloudstorage/client/App.java
@@ -0,0 +1,13 @@
+package com.geekbrains.cloudstorage.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Client World!" );
+    }
+}
